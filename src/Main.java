0075f920d410dd7d405813cb6920ad091bb23dae
jhlee <junhui12345@gmail.com>
2024-03-11 11:28:25 +0900

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("HI");
+        System.out.println("HI");
+        System.out.println("HI");
+        System.out.println("HI");
+        System.out.println("HI");
+        System.out.println("HI");
+        System.out.println("HI");
+        System.out.println("HI");
+        System.out.println("HI");
+        System.out.println("HI");
     }
 }
